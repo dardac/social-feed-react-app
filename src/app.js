87b8b9e01d80c795428cdab5a96cd91c84fe8d52
dardac <dardacastilloh@gmail.com>
2019-10-03 02:46:20 -0400

    Social Feed react app finished

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SocialFeedView from './pages/SocialFeedView/SocialFeedView'
+
+class App extends Component {
+
+    render() {
+        return (
+            <SocialFeedView />
+        )
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+ReactDOM.render(<App />, document.getElementById('app'))
